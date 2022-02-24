2f2c11fd93d9f3fcfe1c8bebe536fba17eff04b7
ahmetsyildirim <ahmets.yildirim@gmail.com>
2022-02-24 16:45:41 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 # Configure the Microsoft Azure Provider
 terraform {
-  required_providers {
+    backend "azurerm" {
+  }
+   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
       version = "~>2.0"
     }
-  }
-  backend "azurerm" {
   }
 }
   
